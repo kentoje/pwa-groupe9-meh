8caa902296c2a670e442ea9a0c57e4ebe51ae658
kentoje <k.monthubert@gmail.com>
2020-05-18 22:59:54 +0200

    feat(Header): Add Header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="nav">
+    <HeaderLeft class="nav__parts nav__parts--left" />
+    <div class="logo">
+      <span>Logo</span>
+    </div>
+    <HeaderRight class="nav__parts nav__parts--right" />
+  </nav>
+</template>
+
+<style lang="scss">
+  .nav {
+    position: fixed;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100vw;
+    padding: 18px 0;
+
+    &__parts--left {
+      margin-left: 180px;
+    }
+
+    &__parts--right {
+      margin-right: 180px;
+    }
+
+    .logo {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: calc(100% / 3);
+      font-size: 14px;
+    }
+  }
+</style>
+
+<script>
+import HeaderLeft from '@/components/Header/Left/HeaderLeft.vue';
+import HeaderRight from '@/components/Header/Right/HeaderRight.vue';
+
+export default {
+  name: 'Header',
+  components: {
+    HeaderLeft,
+    HeaderRight,
+  },
+};
+</script>
---
+++ src/components/Header/Left/HeaderLeft.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="router">
+    <router-link class="router__links" to="/">Home</router-link>
+    <router-link class="router__links" to="/listing">Listing</router-link>
+  </div>
+</template>
+
+<style lang="scss">
+  .router {
+    width: calc(100% / 3);
+
+    &__links {
+      margin: 0 20px;
+      color: #444952;
+      font-size: 14px;
+      text-transform: uppercase;
+      text-decoration: none;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: 'HeaderLeft',
+};
+</script>
